4c8e10e0ce9f0eeefd05563b76bff3f5a7736230
EricMai2112 <mait58674@gmail.com>
2026-08-09 11:58:24 +0700

    feat/#: mongodb configuration and set up cors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
