85ea07b1261ad6a7972f9cd16913c829dd0202c2
Binard2112 <mait58674@gmail.com>
2026-08-10 16:46:29 +0700

    feat/#: convert architecture for modular

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
-  exports: [MongooseModule],
+  exports: [UserService],
   controllers: [UserController],
   providers: [UserService]
 })
