43174849082a06cfb1f3640c02987514ce90672a
Binard2112 <mait58674@gmail.com>
2026-08-10 15:27:47 +0700

    feat/#: handle register and login for users, set up validation pipe for validation

--- backend/src/modules/user/controllers/user.controller.ts
+++ backend/src/modules/user/controllers/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { UserService } from '../services/user.service';
+import { LoginDto, RegisterDto } from '../dtos/user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post("register")
+    @UsePipes(new ValidationPipe())
+    async register(@Body() registerDto: RegisterDto) {
+        return this.userService.register(registerDto)
+    }
+
+    @Post("login")
+    @UsePipes(new ValidationPipe())
+    async login(@Body() loginDto: LoginDto) {
+        return this.userService.login(loginDto)
+    }
+}
